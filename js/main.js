6a28ff43161bee27fd023a95b145008d19f40dae
Jahangir Abdullayev <jk4abdl@gmail.com>
2020-10-18 17:55:31 -0400

    still a mess :frowning:
    i just wanna start from scratch

--- js/main.js
+++ js/main.js
@@ -45,21 +45,21 @@ ready(function () {
 });
 
 
-// ~--------- POP-UP saying things are stil in the working ----------~
-// Get the modal
-var modal = document.getElementById("myModal");
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("closeM")[0];
-// When the user clicks on <span> (x), close the modal
-span.onclick = function () {
-  modal.style.display = "none";
-}
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function (event) {
-  if (event.target == modal) {
+// ~--------- POP-UP saying things are stil in the working ----------~ //
+function inWork() {
+  // Get the modal
+  var modal = document.getElementById("myModal");
+  // Get the <span> element that closes the modal
+  var span = document.getElementsByClassName("closeM")[0];
+  // When the user clicks on <span> (x), close the modal
+  span.onclick = function () {
     modal.style.display = "none";
   }
-}
-function inWork() {
+  // When the user clicks anywhere outside of the modal, close it
+  window.onclick = function (event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
+  }
   modal.style.display = "block";
 }
