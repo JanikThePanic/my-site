6e3f9b444ca02443e9dc3f0e46270f342b2e4c65
Jahangir Abdullayev <jk4abdl@gmail.com>
2020-11-09 23:23:50 -0500

    Working on Thoughts page :helicopter:

---
+++ js/thoughts.js
@@ -0,0 +1,42 @@
+// load xml file and send requests and stuff
+function loadXML() {
+    // make new http request, its a js thing
+    var xmlhttp = new XMLHttpRequest();
+
+    // GET, file location and name, and some other propertie i forget
+    xmlhttp.open("GET", "/assets/tempThoughts.xml", true);
+    xmlhttp.send();
+
+    // when there is a change in the request's state, itll check all is green and run the table loading function
+    xmlhttp.onreadystatechange = function(){
+        // all green
+        if (this.readyState == 4 && this.status == 200) {
+            loadEntries(this);
+        }
+        // cant find the xml
+        if(this.status == 404) {
+            console.log("couldn't find the xml file")
+        }
+    }
+}
+
+// function that loops through the data and spins it into the html. required the "this" from the request made above
+function loadEntries(xml) {
+    var file = xml.responseXML;
+    var loaded="";
+    var pulledData = file.getElementsByTagName("entry");
+
+    // loops through all <resource>'s and addes them to the great table varible
+    for (i = 0; i < pulledData.length; i++) {
+        loaded += "<tr><td>" +
+        pulledData[i].getElementsByTagName("title")[0].childNodes[0].nodeValue +
+        "</td><td>" +
+        pulledData[i].getElementsByTagName("thought")[0].childNodes[0].nodeValue +
+        "</td><td>" +
+        pulledData[i].getElementsByTagName("type")[0].childNodes[0].nodeValue +
+        "</td></tr>";
+    }
+    document.getElementById("ThoughtsGoHere").innerHTML = loaded;
+    // by default sort the table by latest thought
+    //sortTable(0);
+}
