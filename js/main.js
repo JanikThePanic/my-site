b87d71496363fd4793f7183719538daeb678b2a5
Jahangir Abdullayev <jk4abdl@gmail.com>
2020-11-15 18:25:06 -0500

    Merged nav.js with main.js and changed global link stuff :palm_tree:

--- js/main.js
+++ js/main.js
@@ -1,5 +1,43 @@
 // main js file used for all pages
 
+// uwu
+// code to include an html file in another html file
+// the fuction takes in the name of the attribute we'll be looking for
+function includeHTML(lookingFor) {
+  var allElements = document.getElementsByTagName("*"); // pulls all elements from html
+  var file; // will store the location of the html we're pulling in
+  var xhttp; // will be the XMLHttpRequest
+
+  // will loop through all the elements until it find one with the attribute "file"
+  for (const element of allElements) {
+    file = element.getAttribute(lookingFor); // sets "file" to the attribute "file" of the current element
+
+    // if the current element HAS a "file", it will not be null, and thereby will run the if
+    if (file) {
+      xhttp = new XMLHttpRequest(); // makes new httprequest
+
+      // once the state of the request changes it'll run the function
+      xhttp.onreadystatechange = function() {
+        // all green
+        if (this.readyState == 4 && this.status == 200) {
+          element.innerHTML = this.responseText;
+        }
+        // the file couldnt be found
+        else if (this.status == 404) {
+          element.innerHTML = "Page not found.";
+          console.log("Page not found.")
+        }
+      }
+      // some js request stuff
+      // it GETs the "file", file being the loction of the file
+      xhttp.open("GET", file, true);
+      xhttp.send();
+      // exit the function, we found "file" and pulled it in
+      return;
+    }
+  }
+}
+
 // open and close menu on mobile
 function openNav() {
   document.getElementById("mySidenav").style.height = "100%";
@@ -22,28 +60,6 @@ function ready(fn) {
     });
   }
 }
-
-ready(function () {
-
-  var website = window.location.hostname;
-
-  var internalLinkRegex = new RegExp('^((((http:\\/\\/|https:\\/\\/)(www\\.)?)?'
-    + website
-    + ')|(localhost:\\d{4})|(\\/.*))(\\/.*)?$', '');
-
-  var anchorEls = document.querySelectorAll('a');
-  var anchorElsLength = anchorEls.length;
-
-  for (var i = 0; i < anchorElsLength; i++) {
-    var anchorEl = anchorEls[i];
-    var href = anchorEl.getAttribute('href');
-
-    if (!internalLinkRegex.test(href)) {
-      anchorEl.setAttribute('target', '_blank');
-    }
-  }
-});
-
 
 // ~--------- POP-UP saying things are stil in the working ----------~ //
 function inWork() {
