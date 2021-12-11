21d1f2b405d5ac6506a1480f53091457e6a8acce
Jahangir Abdullayev <jk4abdl@gmail.com>
2021-12-10 22:55:24 -0500

    didnt work :frowning:

--- src/components/pages/work/LargeDisplay.js
+++ src/components/pages/work/LargeDisplay.js
@@ -19,15 +19,9 @@ export default function LargeDisplay(props) {
 	}, []);
 
 	// if we hit the back button on mobile, i just wanna close the current work displayed
-	useEffect(() => {
-		const backHandler = BackHandler.addEventListener(
-			"hardwareBackPress",
-			() => {
-				props.close();
-			}
-		);
-		return () => backHandler.remove();
-	}, []);
+	//
+	// need to figure out
+	//
 
 	// const for easy referance
 	// work display object passed from page compoent
