dc108ff11d96b4ffb1e7b49e27eeabd55b3a1fcf
Jahangir Abdullayev <jk4abdl@gmail.com>
2022-03-18 19:31:09 -0400

    good enough for now :shoe:

--- src/components/pages/home.js
+++ src/components/pages/home.js
@@ -10,73 +10,14 @@ function home() {
 			<h1 className="page-title">jahangir abdullayev.</h1>
 
 			<p>
-				My name is Jahangir Abdullayev, Janik for short. I am a Canadian
-				Highschool student at{" "}
-				<a
-					href="http://bci.wrdsb.ca/"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					Bluevale Collegiate Institute
-				</a>
-				. As well, I am an engineering, design, and programming
-				enthusiast. In time, I have also become a leading student on{" "}
-				<a
-					href="https://2702rebels.com/"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					The 2702 Rebels.
-				</a>
+				Although Jahangir Abdullayev is my full name, Janik remains my
+				preferred legal name, for simplicity’s sake. I am currently an
+				engineering student at _______ University in Ontario Canada.
+				Majoring in _______ I have a great passion for anything
+				robotics-related.
 			</p>
-
-			<p>
-				I created{" "}
-				<a
-					href="https://janikthepanic.github.io/the-repository/"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					the repository
-				</a>
-				, a grant bookmark. More importantly, over the years I have
-				built multiple advanced competitive robots with the{" "}
-				<a
-					href="https://2702rebels.com/"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					2702 Rebels
-				</a>{" "}
-				Team.
-			</p>
-
-			<p>
-				Living in Canada, I know three languages. These are Russian,
-				English, and a work-place level of French. Alongside this, I
-				possess high skill sets in various fields of STEM and
-				technology.
-			</p>
-
-			<p>
-				Throughout high school, I have worked for{" "}
-				<a
-					href="https://uwaterloo.ca/engineering-science-quest/"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					Engineering Science Quest
-				</a>{" "}
-				as a Highschool Leader. On the side, I freelance on{" "}
-				<a
-					href="https://www.fiverr.com/janikdesign"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					Fiverr
-				</a>{" "}
-				doing Computer-Assisted Design and Graphical Design.
-			</p>
+			<p>further details are irrelevant.</p>
+			<p>i like robots.</p>
 		</div>
 	);
 }
