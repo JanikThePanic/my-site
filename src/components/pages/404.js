aba3e06221a8a370b41fb3355f4f72eebd53d349
Jahangir Abdullayev <jk4abdl@gmail.com>
2023-12-23 03:30:56 -0500

    404 link back fixed 🔗

--- src/components/pages/404.js
+++ src/components/pages/404.js
@@ -5,14 +5,14 @@ function NotFoundPage() {
 	return (
 		<div className="page">
 			<Helmet>
-				<title>youre lost</title>
+				<title>404 - janik</title>
 			</Helmet>
 			<h1 className="page-title">404</h1>
 
-			<p>you're lost buddy.</p>
+			<p>you're lost. i am too, it's okay.</p>
 			<p>this page doesn't exist.</p>
 			<p>
-				back to <a href="/index.html">janik.codes</a>.
+				back to <a href="/">janik.codes</a>.
 			</p>
 		</div>
 	);
